6ac887d0b93e18b777b8cb5e66493ecfb5bfbf74
양진성 <anfakt0606@naver.com>
2023-07-14 19:21:21 +0900

    header main 반응형

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,39 @@
+import React, { useState , useEffect } from 'react';
 import '../styles/header.css'
 
 export default function Header() {
+
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavOpen(!isNavOpen);
+    };
+
+    const closeNav = () => {
+        setIsNavOpen(false);
+    };
+
+    useEffect(() => {
+        const handleResize = () => {
+          if (window.innerWidth > 1400) {
+            setIsNavOpen(false);
+          }
+        };
+    
+        window.addEventListener('resize', handleResize);
+    
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
       <header className="header">
         <div className="headerInner">
             <h1><span className="hidden">SRC 로고</span></h1>
-            <nav>
+            <nav className={isNavOpen ? 'on' : ''}>
                 <h2 className="hidden">네비게이션 영역</h2>
+                <button type="button" className="mobile_close_btn" onClick={closeNav}></button>
                 <ul>
                     <li><a href="#howto">HOW TO</a></li>
                     <li><a href="#RoadMap">LOAD MAP</a></li>
@@ -25,6 +52,7 @@ export default function Header() {
                     <span className='hidden'>유튜브</span>
                 </li>
             </ul>
+            <button type="button" className="mobile_btn" onClick={toggleNav}></button>
         </div>
       </header>
     );
