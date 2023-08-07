c581914ce5347e9164916624e3af7523b0116561
양진성 <anfakt0606@naver.com>
2023-08-07 17:47:18 +0900

    header responsive

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,7 @@ import '../styles/header.css'
 export default function Header() {
 
     const [isNavOpen, setIsNavOpen] = useState(false);
+    const [is1400 , setIs1400] = useState(false);
 
     const toggleNav = () => {
         setIsNavOpen(!isNavOpen);
@@ -16,7 +17,10 @@ export default function Header() {
     useEffect(() => {
         const handleResize = () => {
           if (window.innerWidth > 1400) {
-            setIsNavOpen(false);
+            // setIsNavOpen(false);
+            setIs1400(false);
+          }else{
+            setIs1400(true);
           }
         };
     
@@ -28,14 +32,16 @@ export default function Header() {
     }, []);
 
     return (
-      <header className="header">
+      <header className={isNavOpen ? 'header on' : 'header'}>
         <div className="headerInner">
-            <h1><span className="hidden">SRC 로고</span></h1>
-            <button type="button" className="app_btn"><span>DOWNLOAD THE APP FOR A PASSIVE INCOME</span></button>
-            <button type="button" className="mobile_btn" onClick={toggleNav}></button>
-            <div className="menu">
+            <h1 className={is1400 ? 'hidden' : ''}><span className="hidden">SRC 로고</span></h1>
+            <div className="headerBtn">
+            <button type="button" className={is1400 ? 'hidden' : 'appBtn'}><span>DOWNLOAD THE APP FOR A PASSIVE INCOME DOWNLOAD THE APP FOR A PASSIVE INCOME</span></button>
+            <button type="button" className={isNavOpen ? 'mobileBtn on' : 'mobileBtn'} onClick={toggleNav}></button>
+            </div>
+            <div className={isNavOpen ? 'menu on' : 'menu'}>
               <div className="menuInner">
-                <nav className={isNavOpen ? 'on' : ''}>
+                <nav>
                     <h2 className="hidden">네비게이션 영역</h2>
                     <ul>
                         <li><a href="#howto"><span>HOME</span></a></li>
@@ -45,17 +51,38 @@ export default function Header() {
                         <li><a href="#Team"><span>TEAM</span></a></li>
                     </ul>
                 </nav>
-                <ul className="sns">
+                <div className="navLink">
+                  <ul className="sns">
+                      <li>
+                          <span className='hidden'>디스코드</span>
+                      </li>
+                      <li>
+                          <span className='hidden'>트위터</span>
+                      </li>
+                      <li>
+                          <span className='hidden'>깃북</span>
+                      </li>
+                  </ul>
+                  <ul className="introduce">
                     <li>
-                        <span className='hidden'>디스코드</span>
+                      <a  
+                        href="#"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                          Whitepaper
+                      </a>
                     </li>
                     <li>
-                        <span className='hidden'>백서</span>
+                      <a  
+                        href="#"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                          Privacy policy
+                      </a>
                     </li>
-                    <li>
-                        <span className='hidden'>트위터</span>
-                    </li>
-                </ul>
+                  </ul>
+                  <p>COPYRIGHT 2023 SRC CORP. ALL RIGHTS RESERVED<br/>I POWERED BY SRC CORP.</p>
+                </div>
               </div>
             </div>
         </div>
