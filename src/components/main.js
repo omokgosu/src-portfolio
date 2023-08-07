b53633029fdafb2ee4f5f51bbdd5bcbb781784fb
양진성 <anfakt0606@naver.com>
2023-08-07 23:44:24 +0900

    main layout

--- src/components/main.js
+++ src/components/main.js
@@ -1,92 +1,55 @@
-import { useState } from 'react';
+import { useState,useRef,useEffect } from 'react';
 import '../styles/main.css'
 
 export default function Main() {
 
-    const [isHovered, setIsHovered] = useState(false);
+    const [load , setLoad] = useState(false);
 
-    const handleMouseEnter = () => {
-      setIsHovered(true);
-    };
+    useEffect(()=>{
+      setLoad(true);
+    },[])
 
-    const handleMouseLeave = () => {
-      setIsHovered(false);
-    };
     return (
-      <main className="main">
+      <main className={load ? "main on" : "main"}>
         <div className="mainInner">
-            <div className="mainAni">
-              <ul className="mainTitle">
+            <p><span>DRIVE, </span><span>SAFE, </span><span>EARN</span></p>
+            <p><span>_SRC</span><span>Turn your daily driving<br/>into passive income.</span></p>
+            <ul className="mainAppBtn">
                 <li>
-                  <h3>DRIVE</h3>
+                    <a 
+                        href="https://testflight.apple.com/join/a2eEql39"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src='/assets/icons/main/play_store.svg' alt="플레이스토어 이미지"/>
+                        <span>PLAY STORE</span>
+                        <img src='/assets/icons/main/app_arrow.svg' alt="화살표"/>
+                    </a>
                 </li>
                 <li>
-                  <h3>SAFETY</h3>
+                    <a 
+                        href="https://testflight.apple.com/join/a2eEql39"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >   
+                        <img src='/assets/icons/main/app_store.svg' alt="앱스토어 이미지"/>
+                        <span>APP STORE</span>
+                        <img src='/assets/icons/main/app_arrow.svg' alt="화살표"/>
+                    </a>
                 </li>
-                <li>
-                  <h3>EARN</h3>
-                </li>
-              </ul>
-              <div className="road">
-                <span className="roadSide"></span>
-                <ul className="roadMiddle">
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                </ul>
-                <span className="roadSide"></span>
-              </div>
-            </div>
-            <div className="mainInfo">
-              <ul className="mainAppBtn">
-                  <li>
-                      <a 
-                          href="https://testflight.apple.com/join/a2eEql39"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                      >
-                          <img src='/assets/icons/main/play_store.svg' alt="플레이스토어 이미지"/>
-                          <span>PLAY STORE</span>
-                      </a>
-                  </li>
-                  <li
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave} 
-                  >
-                      <a 
-                          href="https://testflight.apple.com/join/a2eEql39"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                      >   
-                          <img src={isHovered ? '/assets/icons/main/app_store_black.svg' : '/assets/icons/main/app_store.svg'} alt="앱스토어 이미지"/>
-                          <span>APP STORE</span>
-                      </a>
-                  </li>
-              </ul>
-              <p>Turn your daily driving into passive income.</p>
-            </div>
-            <ul className="carAnimaition">
-              <li>
-                {/* <img src="/assets/icons/main/Main_animaition_2.svg" alt="car svg"/> */}
-              </li>
-              <li>
-                <img className="car1" src="/assets/icons/main/Main_animaition_1.svg" alt="car svg"/>
-                <img className="car2" src="/assets/icons/main/Main_animaition_1.svg" alt="car svg"/>
-                <img className="car3" src="/assets/icons/main/Main_animaition_1.svg" alt="car svg"/>
-                <img className="car4" src="/assets/icons/main/Main_animaition_1.svg" alt="car svg"/>
-                <img className="car5" src="/assets/icons/main/Main_animaition_1.svg" alt="car svg"/>
-                <img className="car6" src="/assets/icons/main/Main_animaition_1.svg" alt="car svg"/>
-                <img className="car7" src="/assets/icons/main/Main_animaition_1.svg" alt="car svg"/>
-                <img className="car8" src="/assets/icons/main/Main_animaition_1.svg" alt="car svg"/>
-                <img className="car9" src="/assets/icons/main/Main_animaition_1.svg" alt="car svg"/>
-              </li>
             </ul>
+        </div>
+        <div className="road">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className="car">
+          <img src='/assets/icons/main/car_black.png' alt="검정색 차" />
+          <img src='/assets/icons/main/car_blue.png' alt="파란색 차" />
+          <img src='/assets/icons/main/car_pink.png' alt="핑크색 차" />
         </div>
       </main>
     );
