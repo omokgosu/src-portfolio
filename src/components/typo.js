c1e7a63ffc7e75fa747f89a87e797af329a2ac65
양진성 <anfakt0606@naver.com>
2023-08-08 22:38:59 +0900

    type responsive and add howto img

--- src/components/typo.js
+++ src/components/typo.js
@@ -1,35 +1,64 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState , useEffect, useRef } from 'react';
 import '../styles/typo.css'
 
 export default function Typo(){ 
-    const [isOn, setIsOn] = useState(false);
-
-    useEffect(() => {
+  
+    const [load , setLoad] = useState(false);
+    const typoRef = useRef(null);
+    useEffect(()=>{
       const handleScroll = () => {
-        const typoElement = document.querySelector('.typo');
-        const offsetTop = typoElement.offsetTop;
-  
-        // 현재 스크롤 위치가 offsetTop - 50vh보다 크면 클래스를 추가하고, 그렇지 않으면 클래스를 제거합니다.
-        if (window.pageYOffset > offsetTop - window.innerHeight * 0.5) {
-          setIsOn(true);
-        } else {
-          setIsOn(false);
+        const scroll = document.documentElement.scrollTop;
+        const height = window.innerHeight;
+        const current = typoRef.current.offsetTop;
+        console.log(scroll , current-height/2);
+        if(scroll > current-height){
+          setLoad(true);
         }
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-  
+        if(scroll < current-height+100){
+          setLoad(false);
+        }
+      }
+      window.addEventListener('scroll',handleScroll)
+
       return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-  
+        window.removeEventListener('scroll',handleScroll)
+      }
+    },[])
+
     return (
-        <section className={`typo ${isOn ? 'on' : ''}`}>
+        <section ref={typoRef} className={load ? "typo on" : "typo"}>
             <div className="typoInner">
-                <p>SLOW DOWN</p>
-                <h3>GET TOKEN<br/>REWARDS ON THE APP</h3>
-                <p>AND SAFELY<br/>ON THE ROAD</p>
+                <p>GET TOKEN REWARDS</p>
+                <div className="typoApp">
+                  <p>ON THE APP</p>
+                  <ul className="typoAppBtn">
+                    <li>
+                      <a 
+                          href="https://testflight.apple.com/join/a2eEql39"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                      >   
+                          <img src='/assets/icons/main/app_store_black.svg' alt="앱스토어 이미지"/>
+                          <span className="hidden">APP STORE</span>
+                      </a>
+                    </li>
+                    <li>
+                        <a 
+                            href="https://testflight.apple.com/join/a2eEql39"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src='/assets/icons/main/play_store.svg' alt="플레이스토어 이미지"/>
+                            <span className="hidden">PLAY STORE</span>
+                        </a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="typoRoad">
+                  <span className="side"></span>
+                  <span className="middle"></span>
+                  <span className="side"></span>
+                </div>
             </div>
         </section>
     );
