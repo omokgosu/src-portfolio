19233240345df998e0097abd1b3d2121748fb118
양진성 <anfakt0606@naver.com>
2023-08-12 00:48:52 +0900

    TEAM responsive

--- src/components/team.js
+++ src/components/team.js
@@ -1,10 +1,11 @@
+import React, {useState , useEffect, useRef} from 'react';
 import '../styles/team.css'
 
 export default function Team() {
 
     const team = [
         {
-            src: 'hc',
+            src: 'lee',
             name: 'HC Lee (lead)',
             Role: 'Product Manager'
         },
@@ -19,7 +20,7 @@ export default function Team() {
             Role: 'Full-stack dev '
         },
         {
-            src: 'jungmin',
+            src: 'so',
             name: 'Jungmin So',
             Role: 'App front-end'
         },
@@ -39,7 +40,7 @@ export default function Team() {
             Role: 'Blockchain developer'
         },
         {
-            src: 'seungho',
+            src: 'back',
             name: 'Seungho Baek',
             Role: 'Designer'
         },
@@ -55,25 +56,61 @@ export default function Team() {
         }
     ]
 
+    const containerRefs = useRef([]);
+    const [tilt, setTilt] = useState([]);
+  
+    const handleMouseMove = (event, index) => {
+      const containerRect = containerRefs.current[index].getBoundingClientRect();
+      const mouseX = event.clientX - containerRect.left;
+      const mouseY = event.clientY - containerRect.top;
+      const centerX = mouseX - containerRect.width / 2;
+      const centerY = mouseY - containerRect.height / 2;
+      const d = Math.sqrt(centerX ** 2 + centerY ** 2);
+  
+      const updatedTilt = [...tilt];
+      updatedTilt[index] = {
+        x: -centerY / 100,
+        y: -centerX / 100,
+        z: d / 10
+      };
+      setTilt(updatedTilt);
+    };
+  
+    const handleMouseLeave = (index) => {
+      const updatedTilt = [...tilt];
+      updatedTilt[index] = { x: 0, y: 0, z: 0 };
+      setTilt(updatedTilt);
+    };
+
     return (
         <section id="Team">
             <div className="TeamInner">
                 <h3>TEAM</h3>
                 <ul className="member">
-                    {
-                        team &&
-                        team.map(el =>{
-                            return(
-                                <li key={el.src}>
-                                    <img src={`/assets/images/team/team_${el.src}.png`} alt={el.name}/>
-                                    <dl>
-                                        <dt>{el.name}</dt>
-                                        <dd>{el.Role}</dd>
-                                    </dl>
-                                </li>
-                            )
-                        })
-                    }
+                {team &&
+                    team.map((el, i) => {
+                    return (
+                        <li key={el.src}>
+                        <div
+                            ref={(element) => (containerRefs.current[i] = element)}
+                            onMouseMove={(e) => handleMouseMove(e, i)}
+                            onMouseLeave={() => handleMouseLeave(i)}
+                        >
+                            <img
+                            src={`/assets/images/team/team_${el.src}.png`}
+                            alt={el.name}
+                            style={{
+                                transform: `rotate3d(${tilt[i]?.y || 0}, ${tilt[i]?.x || 0}, 0, ${tilt[i]?.z || 0}deg)`
+                            }}
+                            />
+                        </div>
+                        <dl>
+                            <dt>{el.name}</dt>
+                            <dd>{el.Role}</dd>
+                        </dl>
+                        </li>
+                    );
+                    })}
                 </ul>
             </div>
         </section>
