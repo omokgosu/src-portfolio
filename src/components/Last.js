21149dbd54f831c43ca1e1c13c39c8881ebef14c
양진성 <anfakt0606@naver.com>
2023-08-13 00:29:56 +0900

    last responsive

--- src/components/Last.js
+++ src/components/Last.js
@@ -17,7 +17,7 @@ export default function Last() {
         <section className="Last">
             <div className="LastInner">
                 <div className="LastTitle">
-                    <p>Turn your daily driving into passive income.</p>
+                    <p>TURN YOUR DAILY DRIVING<br/>INTO PASSIVE INCOME</p>
                     <ul className="LastAppBtn">
                         <li>
                             <a 
@@ -27,24 +27,27 @@ export default function Last() {
                             >
                                 <img src='/assets/icons/main/play_store.svg' alt="플레이스토어 이미지"/>
                                 <span>PLAY STORE</span>
+                                <img className="arrow" src='/assets/icons/main/app_arrow.svg' alt="화살표"/>
                             </a>
                         </li>
-                        <li
-                            onMouseEnter={handleMouseEnter}
-                            onMouseLeave={handleMouseLeave} 
-                        >
+                        <li>
                             <a 
                                 href="https://testflight.apple.com/join/a2eEql39"
                                 target="_blank"
                                 rel="noopener noreferrer"
                             >   
-                                <img src={isHovered ? '/assets/icons/main/app_store_black.svg' : '/assets/icons/main/app_store.svg'} alt="앱스토어 이미지"/>
+                                <img src='/assets/icons/main/app_store.svg' alt="앱스토어 이미지"/>
                                 <span>APP STORE</span>
+                                <img className="arrow" src='/assets/icons/main/app_arrow.svg' alt="화살표"/>
                             </a>
                         </li>
                     </ul>
                 </div>
-                <img src="/assets/images/last/last_app.png" alt="APP" />
+                <div className="imgArea">
+                    <img src ="/assets/images/last/SDT.png" alt="SDT"/>
+                    <img src="/assets/images/last/last_app.png" alt="APP" />
+                    <img src ="/assets/images/last/SMT.png" alt="SMT"/>
+                </div>
             </div>
         </section>
     );
